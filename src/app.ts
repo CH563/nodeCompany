bf30997048ca74df386e124457c19a5ff40ad4c7
chenliwen <wonderful563@qq.com>
2019-07-14 17:34:15 +0800

    :sparkles: 改用typescript

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-const mysql = require('mysql');
-const http = require('http');
-const url = require('url');
-const qs = require('querystring');
-const day = require('dayjs');
+import * as mysql from 'mysql';
+import * as http from 'http';
+import * as url from 'url';
+import * as qs from 'querystring';
+import * as dayjs from 'dayjs';
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -15,10 +15,10 @@ const connection = mysql.createConnection({
 connection.connect();
 
 // 创建服务
-console.log('创建web')
-http.createServer((request, response) => {
+console.log('创建web');
+http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
     // cors跨域
-    response.setHeader('Access-Control-Allow-Origin', true);
+    response.setHeader('Access-Control-Allow-Origin', '*');
     // header类型
     response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     // 跨域允许的请求方式
@@ -27,26 +27,24 @@ http.createServer((request, response) => {
     const { method } = request;
 
     // 处理post请求
-    if (method === 'POST')
-    {
+    if (method === 'POST') {
         console.log('POST 请求');
         const pathName = request.url;
-        console.log(`接口为：${ pathName }`);
-        let tempResult = '';
+        console.log(`接口为：${pathName}`);
+        let tempResult: string = '';
 
         // 数据开始接入
-        request.addListener('data', (chunk) => {
+        request.addListener('data', chunk => {
             tempResult += chunk;
-        })
+        });
 
         // 数据接入完成
         request.addListener('end', () => {
-            const result = JSON.stringify(qs.parse(tempResult));
+            const result: string = JSON.stringify(qs.parse(tempResult));
             console.log('参数为：');
             console.log(result);
 
-            switch (pathName)
-            {
+            switch (pathName) {
                 case '/sendMessage':
                     console.log('送发留言');
                     break;
@@ -57,34 +55,30 @@ http.createServer((request, response) => {
                     console.log('注册');
                     break;
                 default:
-                    console.log(`找不到 ${ pathName } 模块`);
+                    console.log(`找不到 ${pathName} 模块`);
             }
-        })
+        });
         return;
     }
 
     // 处理get请求
-    if (method === 'GET')
-    {
+    if (method === 'GET') {
         console.log('get请求');
 
         // 解析 url 接口
         const pathName = url.parse(request.url).pathname;
-        console.log(`接口为${ pathName }`);
-        
-        if (pathName === '/getMessage')
-        {
+        console.log(`接口为${pathName}`);
+
+        if (pathName === '/getMessage') {
             console.log('获取留言信息');
             return;
         }
-        if (pathName === '/')
-        {
+        if (pathName === '/') {
             response.writeHead(200, {
                 'Content-Type': 'text/html;charset=UTF-8'
-            })
-            response.write('<h4>服务已开启</h4>')
+            });
+            response.write('<h4>服务已开启</h4>');
         }
         response.end();
-
     }
 }).listen(8888); // 监听8888端口
--- tests/text/index.js
+++
@@ -1,7 +0,0 @@
-$.ajax({
-    type: "post",
-    url: "http://localhost:8888/sendMessage",
-    success: function (response) {
-        
-    }
-});
